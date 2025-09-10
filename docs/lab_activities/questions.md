e81ad13d5a2cde59df741e8135ae289c0bf6ec2f
David <nguyen.dddavid@hotmail.com>
2025-09-09 22:14:39 -0400

    FEAT: Test, new CI

---
+++ docs/lab_activities/questions.md
@@ -0,0 +1,10 @@
+# Questions - Labo0
+
+### Question 1
+> Si l'un des tests échoue à cause d'un bug, comment pytest signale-t-il l'erreur et aide-t-il à la localiser ? Rédigez un test qui provoque volontairement une erreur, puis montrez la sortie du terminal obtenue.
+
+![Reponse Q1](repQ1.png)
+Selon l'image, une section dédiée à l'erreur s'affiche, la raison de l'erreur ainsi que la ligne dans le code de test où l'erreur s'est produite.
+
+### Question 2
+> Que fait GitLab pendant les étapes de "setup" et "checkout? Veuillez inclure la sortie du terminal GitLab CI dans votre réponse.
--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -8,6 +8,22 @@ from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
 # TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 3) == 2
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(4, 3) == 12
+
+def test_division():
+    my_calculator = Calculator()
+    assert my_calculator.division(10, 2) == 5
+    assert my_calculator.division(10, 0) == "Erreur : division par zéro"
